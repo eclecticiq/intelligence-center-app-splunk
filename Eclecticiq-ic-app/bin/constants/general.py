e05638ee04f0666129ac8aca086cd95666c226d8
sangeetha-sacumen <sangeetha.n@sacumen.com>
2023-02-07 00:47:52 +0530

    uploaded cloud support

---
+++ Eclecticiq-ic-app/bin/constants/general.py
@@ -0,0 +1,18 @@
+PAYLOAD = "payload"
+STATUS_STR = "status"
+HEADERS = "headers"
+AUTHORIZATION = "Authorization"
+CREDS = "creds"
+PROXY = "proxy"
+URL = "url"
+STR_COLON = ":"
+
+
+# Proxy Formatter paramters
+PROXY_TYPE_HTTPS = "https"
+PROXY_TYPE_HTTP = "http"
+PROXY_TYPE = "proxy_type"
+PROXY_URL = "proxy_url"
+PROXY_PORT = "proxy_port"
+PROXY_USERNAME = "proxy_username"  # nosec
+PROXY_PASSWORD = "proxy_password"  # nosec
